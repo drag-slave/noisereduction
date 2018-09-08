a1f11880684d972701c8f16c25c5ddd0641d0ee2
drag-slave <44192420+drag-slave@users.noreply.github.com>
2018-09-08 17:05:36 +0900

    Add support for logging

--- noise_reduction.py
+++ noise_reduction.py
@@ -53,25 +53,39 @@ for j in range(min(3, numberOfData)):
 # 入力データを定義
 x = tf.placeholder(tf.float32, [None, numberOfPoints])
 
+# 入力画像をログに出力
+img = tf.reshape(x, [-1, numberOfPoints, 1, 1])
+tf.summary.image("log_input_data", img, 2)
+
 # 入力層から中間層
-w_1 = tf.Variable(tf.truncated_normal(
-    [numberOfPoints, numberOf2ndPerceptrons], stddev=stddevOfPerceptrons), name="w1")
-b_1 = tf.Variable(tf.zeros([numberOf2ndPerceptrons]), name="b1")
-h_1 = tf.nn.relu(tf.matmul(x, w_1) + b_1)
+with tf.name_scope("second_layer"):
+    w_1 = tf.Variable(tf.truncated_normal(
+        [numberOfPoints, numberOf2ndPerceptrons], stddev=stddevOfPerceptrons), name="w1")
+    b_1 = tf.Variable(tf.zeros([numberOf2ndPerceptrons]), name="b1")
+    h_1 = tf.nn.relu(tf.matmul(x, w_1) + b_1)
+
+    # 中間層の重みの分布をログ出力
+    tf.summary.histogram('log_w_1', w_1)
 
 # 中間層から出力層
-w_2 = tf.Variable(tf.truncated_normal(
-    [numberOf2ndPerceptrons, numberOfPoints], stddev=stddevOfPerceptrons), name="w2")
-b_2 = tf.Variable(tf.zeros([numberOfPoints]), name="b2")
-# out = tf.nn.softmax(tf.matmul(h_1, w_2) + b_2)
-out = tf.matmul(h_1, w_2) + b_2
+with tf.name_scope("output_layer"):
+    w_2 = tf.Variable(tf.truncated_normal(
+        [numberOf2ndPerceptrons, numberOfPoints], stddev=stddevOfPerceptrons), name="w2")
+    b_2 = tf.Variable(tf.zeros([numberOfPoints]), name="b2")
+    # out = tf.nn.softmax(tf.matmul(h_1, w_2) + b_2)
+    out = tf.matmul(h_1, w_2) + b_2
 
 # 誤差関数
 y = tf.placeholder(tf.float32, [None, numberOfPoints])
-loss = tf.reduce_mean(tf.square(y - out))
+with tf.name_scope("loss"):
+    loss = tf.reduce_mean(tf.square(y - out))
+
+    # 誤差をログ出力
+    tf.summary.scalar("log_loss", loss)
 
 # 訓練
-train_step = tf.train.GradientDescentOptimizer(learningRate).minimize(loss)
+with tf.name_scope("train"):
+    train_step = tf.train.GradientDescentOptimizer(learningRate).minimize(loss)
 
 # 評価
 # correct = tf.equal(tf.argmax(out,1), tf.argmax(y,1))
@@ -80,7 +94,11 @@ train_step = tf.train.GradientDescentOptimizer(learningRate).minimize(loss)
 # 初期化
 init = tf.global_variables_initializer()
 
+summary_op = tf.summary.merge_all()
+
 with tf.Session() as sess:
+
+    summary_writer = tf.summary.FileWriter("logs", sess.graph)
 
     sess.run(init)
 
@@ -88,7 +106,15 @@ with tf.Session() as sess:
     sess.run(
         train_step, feed_dict={x:obs[0:numberOfTrain], y:signal[0:numberOfTrain]})
 
-    outVal = sess.run(out, feed_dict={x:obs[numberOfTrain:numberOfData]})
+    test_data = obs[numberOfTrain:numberOfData]
+
+# tf.reshapeの第1引数に、numpy.arrayを入れていいのかあやしい。
+    test_images = np.reshape(test_data, [
+        numberOfData - numberOfTrain, numberOfPoints])  # , 1, 1])
+    test_labels = signal[numberOfTrain:numberOfData]
+
+# test_dataからノイズ除去
+    outVal = sess.run(out, feed_dict={x:test_data})
     for i in range(len(outVal)):
         print(str(i) + str(len(outVal[i])))
         plt.scatter(samplePoints, outVal[i], s=numberOfPoints)
@@ -100,3 +126,9 @@ with tf.Session() as sess:
 #             acc_val = sess.run(accuracy ,feed_dict={x:test_images, y:test_labels})
 #             print('Step %d: accuracy = %.2f' % (step, acc_val))
 
+        if i % 1 == 0:
+            # ログを取る処理を実行する（出力はログ情報が書かれたプロトコルバッファ）
+            # test_labelsを2次元arrayで与えていいか不明。
+            summary_str = sess.run(summary_op, feed_dict={x:test_images, y:test_labels})
+            # ログ情報のプロトコルバッファを書き込む
+            summary_writer.add_summary(summary_str, i)
